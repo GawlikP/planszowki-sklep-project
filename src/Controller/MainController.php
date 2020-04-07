b9620f28a33a0514e7a10e48730b67613ccccfad
GawlikP <rudepeeter@gmail.com>
2020-04-07 23:19:57 +0200

    Solve {{test}} problem

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,10 @@ class MainController extends AbstractController{
   public function main(){
 
 
-    return $this->render('main/main.html.twig');
+    return $this->render('main/main.html.twig',
+    [ 'test' => '',
+    ]
+    );
   }
   /**
   * @Route("/blog",name="main_list")
