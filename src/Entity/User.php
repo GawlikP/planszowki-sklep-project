00a5883c1eee7e46a68fbdf07c959f90bb1a9724
GawlikP <rudepeeter@gmail.com>
2020-04-16 20:54:46 +0200

    Register site creation

---
+++ src/Entity/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nick;
+
+    /**
+     * @ORM\Column(type="string", length=512)
+     */
+    private $Password;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Permission;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNick(): ?string
+    {
+        return $this->Nick;
+    }
+
+    public function setNick(string $Nick): self
+    {
+        $this->Nick = $Nick;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): self
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+    public function getPermission(): ?int
+    {
+        return $this->Permission;
+    }
+
+    public function setPermission(int $Permission): self
+    {
+        $this->Permission = $Permission;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20200416183440.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200416183440 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nick VARCHAR(255) NOT NULL, password VARCHAR(512) NOT NULL, permission INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE user');
+    }
+}
