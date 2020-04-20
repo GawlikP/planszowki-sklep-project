989d23bab6e57ed78b0b73847b44db567709afa1
GawlikP <rudepeeter@gmail.com>
2020-04-20 09:00:57 +0200

    Add some example images

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -201,7 +201,7 @@ class MainController extends AbstractController{
     return $response;
   }
 
-  public function basketChange(RequestStack $requeststack, Request $request){
+  public function basketChange(RequestStack $requestStack, Request $request){
 
     $basket = $request->cookies->get('b');
     $requestt = $requestStack->getCurrentRequest();
